b252bc4d0dc3dab6a894e6db3a58e73d51d994bb
ctl <3054008136@qq.com>
2022-07-05 14:03:48 +0800

    feat(api/utils): 跨域,封装

--- src/main.js
+++ src/main.js
@@ -6,4 +6,6 @@ import router from './router'
 import store from './store'
 import './styles/index.scss'
 
+console.log(process.env.VUE_APP_BASE_API)
+
 createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
